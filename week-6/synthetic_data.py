f737c7fbfee88425f38a0aec11c969134c6bf292
Ivan Montejo de Garcini <elburgoimontejo@gmail.com>
2025-12-30 11:24:07 +0200

    Finished synthetic data lab

--- week-6/synthetic_data.py
+++ week-6/synthetic_data.py
@@ -1,11 +1,27 @@
-from llama_index.llms.ollama import Ollama
-import dspy
+import json, time
+from mistralai import Mistral
+import mistralai.models.sdkerror as MistralError
+import pandas as pd
 
-llm = Ollama(model="llama3", request_timeout=60.0)
+api_key = json.load(open("apikeys.json", "r"))["Mistral"]
+client = Mistral(api_key=api_key)
 
-# you can use DSPY (https://github.com/stanfordnlp/dspy), but you can also choose another method of interacting with an LLM
-dspy.settings.configure(lm=llm)
+def mispell(query: str, n = 1):
+    try: # Try to query the Mistral LLM
+        response = client.chat.complete(
+            model="mistral-medium-latest",
+            messages=[{"role": "user", "content": f"Return {n} mispelled versions of the following text: '{query}'. Return only the mispelled versions separated by new lines and nothing else."}]
+        )
+        return response.choices[0].message.content.replace("\n\n", "\n").split("\n")
+    except MistralError.SDKError: # Because I'm using the free tier the service capacity may be exceeded, in that case try again
+        return mispell(query=query, n = n)
 
+queries = pd.read_csv("week-6/web_search_queries.csv")["Query"].tolist()
+mispelled = dict.fromkeys(queries)
+for k in mispelled.keys():
+    mispelled[k] = mispell(k, 5)
+
+open("week-6/mispelledQueries.json", "w").write(json.dumps(mispelled))
 # Task: implement a method, that will take a query string as input and produce N misspelling variants of the query.
 # These variants with typos will be used to test a search engine quality.
 # Example
